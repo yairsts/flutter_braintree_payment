374b93961e3e5fa78c8ea9451a2957e848dce88e
Yair Tsadik <>
2025-03-25 20:28:29 +0200

    no message

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -7,8 +7,19 @@ import Braintree
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
+        debugPrint("\(Bundle.main.bundleIdentifier ?? "").payments")
         BTAppContextSwitcher.sharedInstance.returnURLScheme = "\(Bundle.main.bundleIdentifier ?? "").payments"
         GeneratedPluginRegistrant.register(with: self)
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
+
+    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        debugPrint("url.scheme: \(url.scheme)")
+
+        if url.scheme?.localizedCaseInsensitiveCompare("\(Bundle.main.bundleIdentifier ?? "").payments") == .orderedSame {
+            return BTAppContextSwitcher.sharedInstance.handleOpen(url)
+        }
+        return false
+    }
+     
 }
